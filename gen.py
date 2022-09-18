63a1e9d3cb3d71fe202374817ea05c79ad064440
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-09-18 22:18:10 +0400

    Update gen.py

--- gen.py
+++ gen.py
@@ -19,7 +19,7 @@ try:
 except ImportError:
     
     class ConfigClass:  # minimal config incase you don't have the config.py
-        MAX_DEPTH = 1  # dive no deeper than this for each root URL
+        MAX_DEPTH = 2  # dive no deeper than this for each root URL
         MIN_DEPTH = 1   # dive at least this deep into each root URL
         MAX_WAIT = 5   # maximum amount of time to wait between HTTP requests
         MIN_WAIT = 1    # minimum amount of time allowed between HTTP requests
